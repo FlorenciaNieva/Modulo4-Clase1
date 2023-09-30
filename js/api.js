0d246a427cc8696ade7db52baa9a867f6db31113
FlorenciaNieva <florencianieva930@gmail.com>
2023-09-30 20:28:11 -0300

    División de javascript entre los archivos main.js y api.js

--- js/api.js
+++ js/api.js
@@ -1,11 +1,4 @@
-const $ = (selector) => document.querySelector(selector);
-const $$ = (selector) => document.querySelectorAll(selector);
 const baseURL = "https://rickandmortyapi.com/api";
-
-const showView = (view) => {
-    $$(".view").forEach((view) => view.classList.add("visually-hidden"));
-    $(`#${view}`).classList.remove("visually-hidden");
-};
 
 const getCharacters = () => {
     fetch(`${baseURL}/character`) //promesa
@@ -25,45 +18,5 @@ const getCharacterById = (id) => {
 //     .then((response) => response.json()) // respuesta parseada
 //     .then((data) => console.log(data)); // data
 // };
-
-const showCharacters = (characters) => {
-    $("#charcaters-container").innerHTML = "";
-    for (let { image, name, id } of characters) {
-        $("#charcaters-container").innerHTML += `
-            <div class="card col-3 m-1">
-            <img src="${image}" class="card-img-top mt-2" alt="character-picture" />
-            <div class="card-body">
-                <h5 class="card-title">${name}</h5>
-            </div>
-            <a href="#" class='btn btn-info mb-2' onclick=getCharacterById(${id})>Ver detalles</a>
-        </div>
-        `;
-    }
-};
-
-const showCharacterDetail = ({
-    name,
-    image,
-    status,
-    species,
-    gender,
-    origin,
-}) => {
-    showView("character-detail");
-    $("#character-detail").innerHTML = `
-    <div class="card col-4">
-        <img src="${image}" class="card-img-top mt-2" alt="character-picture" />
-        <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <ul>
-                <li>Estado: ${status}</li>
-                <li>Especie: ${species}</li>
-                <li>Genero: ${gender}</li>
-                <li>Origen: ${origin.name}</li>
-            </ul>
-        </div>
-        <a href="#" class='btn btn-danger mb-2' onclick=showView("charcaters-container")>Volver</a>
-    </div>`;
-};
 
 window.onload = getCharacters();
