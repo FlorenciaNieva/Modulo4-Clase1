e3277d316caa0d36fa77395ce03f1368c3ae575c
FlorenciaNieva <florencianieva930@gmail.com>
2023-10-01 19:11:48 -0300

    Práctica de la clase 2, del modulo 4 de asincronismo, sobre paginación y filtros de busqueda

--- js/api.js
+++ js/api.js
@@ -3,7 +3,8 @@ const baseURL = "https://rickandmortyapi.com/api";
 const getCharacters = () => {
     fetch(`${baseURL}/character`) //promesa
         .then((response) => response.json()) // respuesta parseada
-        .then((data) => showCharacters(data.results)); // data
+        .then((data) => showCharacters(data.results)) // data
+        // .catch((error) => alert(error)); solamente para respuestas que vienen en rejected
 };
 
 const getCharacterById = (id) => {
@@ -19,4 +20,16 @@ const getCharacterById = (id) => {
 //     .then((data) => console.log(data)); // data
 // };
 
-window.onload = getCharacters();
+const pagination = (page) => {
+    fetch(`${baseURL}/character/?page=${page}`)
+    .then((response) => response.json())
+    .then((data) => showCharacters(data.results));
+};
+
+const getFilterCharacters = (params) => {
+    fetch(`${baseURL}/character/?${params}`)
+        .then((response) => response.json())
+        .then((data) => showCharacters(data.results));
+}
+
+window.onload = () => getCharacters();
--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
+$('#home-btn').addEventListener('click', () => getCharacters());
 
 const showView = (view) => {
     $$(".view").forEach((view) => view.classList.add("visually-hidden"));
     $(`#${view}`).classList.remove("visually-hidden");
 };
 
+let page = 1;
+
 const showCharacters = (characters) => {
-    $("#charcaters-container").innerHTML = "";
-    for (let { image, name, id } of characters) {
-        $("#charcaters-container").innerHTML += `
+    $("#characters-container").innerHTML = "";
+    if (characters) {
+        showView('characters-container');
+        for (let { image, name, id } of characters) {
+        $("#characters-container").innerHTML += `
             <div class="card col-3 m-1">
             <img src="${image}" class="card-img-top mt-2" alt="character-picture" />
             <div class="card-body">
                 <h5 class="card-title">${name}</h5>
             </div>
-            <a href="#" class='btn btn-info mb-2' onclick=getCharacterById(${id})>Ver detalles</a>
+            <a href="#" class='btn btn-info mb-2' onclick=getCharacterById(${id})>See details</a>
         </div>
         `;
+        }
+    } else {
+        showView('no-results');
     }
 };
 
@@ -43,6 +51,55 @@ const showCharacterDetail = ({
                 <li>Origen: ${origin.name}</li>
             </ul>
         </div>
-        <a href="#" class='btn btn-danger mb-2' onclick=showView("charcaters-container")>Volver</a>
+        <a href="#" class='btn btn-danger mb-2' onclick="showView('characters-container')">Go back</a>
     </div>`;
 };
+
+// PAGINACIÓN
+const goNext = () => {
+    if (page < 42) {
+        page = page + 1;
+        pagination(page);
+        $('#page-number').innerHTML = `${page}`;
+    }
+}
+
+const goPrew = () => {
+    if (page > 1) {
+        page = page - 1;
+        pagination(page);
+        $('#page-number').innerHTML = `${page}`;
+    }
+}
+
+// Desplaza la página hacia el inicio al hacer click en los botones
+const pageTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Desplazamiento suave (scroll suave)
+    });
+}
+
+$('#page-number').innerHTML = `${page}`;
+
+$('#prew-btn').addEventListener('click', () => {
+    goPrew()
+    pageTop();
+});
+
+$('#next-btn').addEventListener('click', () => {
+    goNext()
+    pageTop();
+});
+
+//FILTROS DE BUSQUEDA
+const filterCharacter = () => {
+    let params = {
+        name: $('#name-input').value,
+        gender: $('#gender-select').value,
+        status: $('#status-select').value,
+    };
+    getFilterCharacters(new URLSearchParams(params).toString());
+}
+
+$('#filters-btn').addEventListener('click', () => filterCharacter());
